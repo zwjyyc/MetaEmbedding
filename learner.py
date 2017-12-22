260a8c4315cb585e954644422fa94248c0018e3e
Yichun Yin <ssggjjdd@163.com>
2017-12-22 10:32:20 +0800

    bug fixed

--- learner.py
+++ learner.py
@@ -13,7 +13,8 @@ def build_dic(files, dict_file):
             for line in fin:
                 cnt += 1
                 items = line.strip().split()
-                assert cnt == 1 and len(items) == 2, 'the first line must contain numbers of words and dimension'
+                if cnt == 1:
+                    assert len(items) == 2, 'the first line must contain numbers of words and dimension'
                 if items[0] not in word_dic:
                     size = len(word_dic)
                     word_dic[items[0]] = size
